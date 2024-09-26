b676c5da6ebde1d25c321c7fd1fe92f7d5243165
Dan Bock <danbock@gmail.com>
2024-09-26 19:32:08 -0400

    Add oauth token and 100-game limit to Lichess

--- app/collections/[id]/actions/importLichessGames.ts
+++ app/collections/[id]/actions/importLichessGames.ts
@@ -7,61 +7,68 @@ const importLichessGames = async (
   lastRefreshed: Date | null,
   username: string,
 ) => {
-  console.time('insertLichessGames')
+  console.time('importLichessGames')
   const supabase = createServerClient()
 
   const url = `https://lichess.org/api/games/user/${username}?${new URLSearchParams({
+    max: '100',
     since: `${lastRefreshed?.getTime() ?? ''}`,
     moves: 'false',
     opening: 'true',
     lastFen: 'true',
   })}`
 
-  const qr = await fetch(url, {headers: {accept: 'application/x-ndjson'}})
+  const qr = await fetch(url, {
+    headers: {accept: 'application/x-ndjson', Authorization: 'Bearer ' + process.env.LICHESS_TOKEN},
+  })
+
+  console.log('HTTP status', qr.status, qr.statusText)
   const text = await qr.text()
-  console.timeLog('insertLichessGames', `fetched ${text.length} bytes`)
+  console.timeLog('importLichessGames', `fetched ${text.length} bytes`)
 
   const data: Game[] = text
     .split(/\r?\n/)
     .map((l) => (l.trim() ? JSON.parse(l) : null))
     .filter((l) => l)
 
-  try {
-    const {error} = await supabase.from('games').insert(
-      data.map<Database['public']['Tables']['games']['Insert']>((g) => ({
-        site: 'lichess',
-        collection_id: collectionId,
-        eco: g.opening.name,
-        fen: g.lastFen,
-        game_dttm: new Date(g.createdAt).toISOString(),
-        clock_initial: g.clock.initial,
-        clock_increment: g.clock.increment,
-        lichess_game_id: g.id,
-        white_username: g.players.white.user.name,
-        black_username: g.players.black.user.name,
-        white_rating: g.players.white.rating,
-        black_rating: g.players.black.rating,
-        winner: g.winner,
-      })),
-    )
+  if (data.length > 0) {
+    try {
+      const {error} = await supabase.from('games').insert(
+        data.map<Database['public']['Tables']['games']['Insert']>((g) => ({
+          site: 'lichess',
+          collection_id: collectionId,
+          eco: g.opening.name,
+          fen: g.lastFen,
+          game_dttm: new Date(g.createdAt).toISOString(),
+          clock_initial: g.clock.initial,
+          clock_increment: g.clock.increment,
+          lichess_game_id: g.id,
+          white_username: g.players.white.user.name,
+          black_username: g.players.black.user.name,
+          white_rating: g.players.white.rating,
+          black_rating: g.players.black.rating,
+          winner: g.winner,
+        })),
+      )
 
-    console.timeLog('insertLichessGames', 'inserted')
+      console.timeLog('importLichessGames', 'inserted')
 
-    if (error) {
-      console.log('Error inserting games for Lichess')
-      console.error(error)
-    } else {
-      await supabase
-        .from('collections')
-        .update({last_refreshed: new Date().toISOString()})
-        .eq('id', collectionId)
+      if (error) {
+        console.log('Error inserting games for Lichess')
+        console.error(error)
+      } else {
+        await supabase
+          .from('collections')
+          .update({last_refreshed: new Date().toISOString()})
+          .eq('id', collectionId)
+      }
+    } catch (e) {
+      console.log('Caught error inserting games for Lichess')
+      console.error(e)
     }
-  } catch (e) {
-    console.log('Caught error inserting games for Lichess')
-    console.error(e)
   }
 
-  console.timeEnd('insertLichessGames')
+  console.timeEnd('importLichessGames')
   revalidatePath(`/collections/${collectionId}`)
 }
 
