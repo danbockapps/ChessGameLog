4e8d74df0d66fd78e9bdc90c72bc0e7d8d49ff08
Dan Bock <danbock@gmail.com>
2024-10-07 08:52:36 -0400

    Remove old test from Home

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,11 @@ import {redirect} from 'next/navigation'
 import {createServerClient} from './lib/supabase/server'
 
 export default async function Home() {
-  console.log('Home')
-  console.time('fetch')
-  const lichessData = await fetch('https://lichess.org/api/player').then((res) => res.json())
-  console.timeEnd('fetch')
-
   const supabase = createServerClient()
 
   const {data} = await supabase.auth.getUser()
 
   if (data?.user) redirect('/collections')
 
-  return <main>{JSON.stringify(lichessData)}</main>
+  return <main>You are not logged in.</main>
 }
