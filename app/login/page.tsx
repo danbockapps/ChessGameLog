78db5ff9100e8d26fc542d5e9fe08b46350322b6
Dan Bock <danbock@gmail.com>
2024-11-21 19:38:39 -0500

    Make login look nice

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,6 @@
-import Button from '../ui/button'
-import {login, signup} from './actions'
+import {FC} from 'react'
+import LoginSignup from '../ui/loginSignup'
+import {login} from './actions'
 
-export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <Button formAction={login}>Log in</Button>
-      <Button formAction={signup}>Sign up</Button>
-    </form>
-  )
-}
+const LoginPage: FC = () => <LoginSignup buttonText="Log in" onSubmit={login} />
+export default LoginPage
